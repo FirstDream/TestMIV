e1bfe022ce6c94448cf5aa92a9ace6343ce25507
Herman Song <1217019920@qq.com>
2017-05-05 16:20:56 +0800

    abc

--- Graphics/Transform.h
+++ Graphics/Transform.h
@@ -1,11 +1,15 @@
 /*
 表示一个变换矩阵;
+线性变换：
+平移缩放旋转。
 */
 
 
 #pragma once
 
 #include <QObject>
+#include <glm\vec3.hpp>
+#include <glm\mat4x4.hpp>
 
 class Transform : public QObject
 {
@@ -14,5 +18,29 @@ class Transform : public QObject
 public:
 	Transform(QObject *parent);
 	~Transform();
+	Transform(const Transform& trans);
+	Transform& operator=(const Transform& rhs);
 
+
+	void identity();
+	void inverse();
+
+
+	void setMatrix(const glm::mat4& mat);
+	const glm::mat4& getMatrix() const { return this->mat_; }
+	
+	void preMultipy(const Transform& t);
+	void postMultipy(const Transform& t);
+	void preMultipy(const glm::mat4& mat);
+	void postMultipy(const glm::mat4& mat);
+
+	void translate(glm::vec3& xyz);
+	void scale(glm::vec3 scale_xyz);
+	void rotate(float angle, glm::vec3& axis);
+	
+
+private:
+	glm::mat4 mat_;
+	
 };
+Transform operator*(const Transform& lhs, const Transform& rhs);
--- MedicalImagingViewer/main.cpp
+++ MedicalImagingViewer/main.cpp
@@ -10,51 +10,10 @@ int main(int argc, char *argv[])
 	QApplication a(argc, argv);
 	a.setAttribute(Qt::AA_UseDesktopOpenGL, true);
 	
-	QSettings settings(QApplication::applicationDirPath() + "/config.ini", QSettings::IniFormat);
-	settings.beginGroup("logins");
-	settings.setValue("app", QVariant(1));
-	settings.endGroup();
-
-	struct Login {
-		QString userName;
-		QString password;
-	};
-	QList<Login> logins;
-
-	Login l1{ "a","1" };
-	Login l2{ "b","2" };
-	Login l3{ "c","3" };
-
-	logins << l1 << l2 << l3;
-
-	settings.beginWriteArray("logins");
-	for (int i = 0; i < logins.size(); ++i) {
-		settings.setArrayIndex(i);
-		settings.setValue("userName", logins.at(i).userName);
-		settings.setValue("password", logins.at(i).password);
-	}
-	settings.endArray();
-
-	QList<Login> logins2;
-	int size = settings.beginReadArray("logins");
-	for (int i = 0; i < size; ++i) {
-		settings.setArrayIndex(i);
-		Login login;
-		login.userName = settings.value("userName").toString();
-		login.password = settings.value("password").toString();
-		logins2.append(login);
-	}
-	settings.endArray();
-
-	settings.beginWriteArray("logins");
-	{
-		settings.setArrayIndex(0);
-		settings.setValue("userName", "e");
-		settings.setValue("password", "5");
-	}
-	settings.endArray();
-
 	MainWindow w;
 	w.show();
 	return a.exec();
+
+
+
 }
